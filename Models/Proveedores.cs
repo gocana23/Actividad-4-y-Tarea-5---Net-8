e9aa71800e7f2371fb8c5e831bd26ff2711e2eec
gocana23 <gaocanaa@gmail.com>
2024-10-25 23:33:36 -0600

    Actividad_4yTarea5_Net_8

---
+++ Models/Proveedores.cs
@@ -0,0 +1,12 @@
+namespace ApiEmpresa.Models;
+
+public class Proveedores{
+    public Int32 Id { get; set; }
+    public string? nit { get; set; }
+    public string? proveedor { get; set; }
+    public string? direccion { get; set; }
+    public string? razon_social { get; set; }
+    public DateTime? fecha_creacion { get; set; }
+    
+
+}
